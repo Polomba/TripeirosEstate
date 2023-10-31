3a0aed7f9143a31db830ab341f5f0c740b476fe4
Polomba <30212@aeaf.edu.pt>
2023-10-31 14:34:52 +0000

    Update Version 34

--- HomeBalance/app/src/main/java/com/example/homebalance/MainActivity.kt
+++ HomeBalance/app/src/main/java/com/example/homebalance/MainActivity.kt
@@ -16,23 +16,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val revealPass = findViewById<EditText>(R.id.txtPass)
-        revealPass.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // Não é necessário implementar
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // Não é necessário implementar
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                // Atualize o campo de texto com asteriscos
-                val password = s.toString()
-                val maskedPassword = "*".repeat(password.length)
-                revealPass.setText(maskedPassword)
-                revealPass.setSelection(maskedPassword.length)
-            }
-        })
         }
 }
